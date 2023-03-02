747c157389dca4f1d734d786130b26959dffdbcd
Chad Wood <mr.chadmwood@gmail.com>
2023-03-01 17:01:54 -0800

    Delete credman.py

--- credman.py
+++
@@ -1,8 +0,0 @@
-class KeePassMan:
-    def __init__(self, db_dir, *args, **kwargs):
-        from pykeepass import PyKeePass
-        self.cred_retriever = PyKeePass(db_dir, *args, **kwargs)
-
-    # Retrieves credentials
-    def credential_manager(self, secret_id):
-        return self.cred_retriever.find_entries(title=secret_id, first=True)
